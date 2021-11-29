36bd7794c2ce4473f6a9e5068ee27c5826b1ffbb
Aleksandr-Glebik <glebikaa@mail.ru>
2021-11-29 12:41:22 +0300

    feat: add css loader and css style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import AppService from './modules/app.service'
 import { config } from './modules/config'
 import './modules/header.component'
+import './css/index.css'
+
+console.log(css.toString());
 
 console.log('Hello world!');
 console.log('Config key: ', config.key )
